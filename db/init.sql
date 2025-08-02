5a163e33e4fe9d1a540900835453ef61772dce73
orelmuseri1 <orel.museri1@gmail.com>
2025-08-02 19:54:53 +0300

    first commit

---
+++ db/init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price NUMERIC NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS purchases (
+    id SERIAL PRIMARY KEY,
+    user_id UUID NOT NULL,
+    supermarket_id INT NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    total_amount NUMERIC NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS purchase_items (
+    id SERIAL PRIMARY KEY,
+    purchase_id INT REFERENCES purchases(id),
+    product_id INT REFERENCES products(id)
+);
+
+COPY products(name, price)
+FROM '/docker-entrypoint-initdb.d/Products_list.csv'
+DELIMITER ','
+CSV HEADER;
